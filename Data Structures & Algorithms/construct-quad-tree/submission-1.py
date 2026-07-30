2ee2a98bb2e957ecfad0b20a0c3695567b57007e
Sagar Das <Sagar.uiet8@gmail.com>
2026-07-30 18:16:47 -0400

    Add: construct-quad-tree - submission-1

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-1.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+
+class Solution:
+    def construct(self, grid: List[List[int]]) -> 'Node':
+
+        def dfs(n, r, c):
+            same = True
+
+            for i in range(n):
+                for j in range(n):
+                    if grid[r][c] != grid[r + i][c + j]:
+                        same = False
+                        break
+            
+            if same:
+                return Node(grid[r][c], True)
+            
+            n = n // 2
+
+            tl = dfs(n, r, c)
+            tr = dfs(n, r, c + n)
+            bl = dfs(n, r + n, c)
+            br = dfs(n, r + n, c + n)
+
+            return Node(0, False, tl, tr, bl, br)
+
+        return dfs(len(grid), 0, 0)
